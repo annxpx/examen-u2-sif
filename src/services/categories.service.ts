92d22cd6cf291754f6aa4b18ab9793fbd11bb932
Sary Nog <sary.noguera@unah.hn>
2022-11-06 13:49:00 -0600

    Categories Controller: Rutas

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -1,25 +1,34 @@
+import {Request, Response} from 'express';
 import {writeFileSync, readFileSync, appendFileSync} from "node:fs";
 
 class CategoriesService{
 
-     private diccionario= [];
+     private diccionario= [
+        {id: 1, name: 'Escritorios', description: ""},
+        {id: 2, name: 'Computadoras', description: ""},
+     ];
 
      constructor(){
-        const path = "./categories.json";
+        const path = './categories.json';
         const fs = require("fs");
-        this.diccionario = fs.JSON.parse(readFileSync(path, "utf8"));
+        let rawdata = fs.readFileSync('../categories.json');
+        let categories = JSON.parse(rawdata);
+        console.log(categories);
+        //this.diccionario = JSON.parse(readFileSync("./words.json", "utf8"));
      }
 
-    public getList(){
+    public async getList(){
         return this.diccionario;
     } 
 //investigar como caer el write
 //cambiar el enfoque de este codigo de categorias a palabras
-    public create (){
+    /*public create (){
     let id = Math.floor(Math.random() * 25);
     const path = "./categories.json";
     const data = "";
     const fs = require("fs");
-    this.diccionario = fs.JSON.parse(writeFileSync(path,data,"utf8"));
-    }
+    //this.diccionario = fs.JSON.parse(writeFileSync(path,data,"utf8"));
+    }*/
 }
+
+export default new CategoriesService();

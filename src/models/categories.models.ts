12c75956ae242fe0a20f8b48b965587ac88a2660
annxpx <apalvaradol@outlook.com>
2022-11-06 13:54:58 -0600

    tsjsonconfig add

--- src/models/categories.models.ts
+++ src/models/categories.models.ts
@@ -1,20 +1,20 @@
 import 'reflect-metadata';
 import 'es6-shim';
-import 'class-transformer';
+import {plainToClass} from 'class-transformer';
 import 'class-validator';
 export class CategoriesModel{
         id: number;
         name: string;
         words: [string];
       
-        getCategory() {
+        public getCategory() {
           return this.name + ' ' + this.words;
         }
 
 }
+fetch('/src/services/categories.json').then((categories: Object[]) => {
+  const realCategories = plainToClass(CategoriesModel, categories)
+    categories[0].getCategory
+});
 
     
-fetch("/src/services/categories.json").then((categories: Object[]) => {
-    const realCategories = plainToClass(CategoriesModel, categories);
-    // now each user in realUsers is an instance of User class
-  });
